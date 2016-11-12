85155c820c261cbb8c9d49ac4fb462db7083d015
Eric Hasselbring <ehasselbring@gmail.com>
2016-11-12 18:33:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(num)
+    if num < 2
+        return false
+    end
+    prime = true
+    range = []
+    counter = 2
+    while counter < num
+    	range << counter
+    	counter += 1
+    end
+    counter = 2
+    while prime && counter < num / 2 + 1
+        range.each do |r|
+        	if !(range[counter])
+        		next
+        	end
+            if r * range[counter] == num
+                prime = false
+            end
+        end
+        counter += 1
+    end
+    return prime
+end
